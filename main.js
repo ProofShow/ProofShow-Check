5834b919c3f5f80c67cc737277bd7a984ed65807
Tim Liu <hylswind@gmail.com>
2019-06-18 09:46:26 +0800

    Check if there is new version when application startup

--- main.js
+++ main.js
@@ -18,6 +18,40 @@ global.tmpFolder = path.join(os.tmpdir(), app.getName());
 global.psesData = null;
 
 /**
+ * Check application if there is new version
+ * @return {boolean} return true if there is new version
+ */
+ async function checkAppUpdate() {
+
+   try {
+     // load latest version
+     var fetchRes = await fetch('https://check.proof.show/download/version.json');
+
+     if (fetchRes.ok) {
+       var isUpdate = false;
+       var latestVer = (await fetchRes.json()).version.split('.');
+       var currentVer = app.getVersion().split('.');
+
+       for (let verIdx = 0; verIdx < latestVer.length; verIdx++) {
+         if (parseInt(currentVer[verIdx]) > parseInt(latestVer[verIdx]))
+          break;
+         else if (parseInt(currentVer[verIdx]) < parseInt(latestVer[verIdx])) {
+           isUpdate = true;
+           break;
+         }
+       }
+
+       return isUpdate;
+     } else {
+       return false;
+     }
+   } catch(err) {
+     console.log(err);
+     return false;
+   }
+ }
+
+/**
  *  Retrieve the PSES data
  */
 async function retrievePSES() {
@@ -87,102 +121,111 @@ async function checkReceipt(receiptInfo) {
 /**
  *  Initaite ProofShow Check application
  */
-function initApp() {
-  // Create the browser window.
-  global.mainWindow = new BrowserWindow({
-    width: 500,
-    height: 300,
-    resizable: false,
-    maximizable: false,
-    webPreferences: {
-      nodeIntegration: true,
-      devTools: false
-    }
-  });
-  global.mainWindow.setMenuBarVisibility(false);
+async function initApp() {
+  if (await checkAppUpdate()) {
+    dialog.showMessageBox({
+      type: 'none',
+      message: 'There is new version for ProofShow Check, please upgrade it.',
+      buttons: ['OK']
+    });
+    app.quit();
+  } else {
+    // Create the browser window.
+    global.mainWindow = new BrowserWindow({
+      width: 500,
+      height: 300,
+      resizable: false,
+      maximizable: false,
+      webPreferences: {
+        nodeIntegration: true,
+        devTools: false
+      }
+    });
+    global.mainWindow.setMenuBarVisibility(false);
 
-  if (process.env.NODE_ENV === 'development')
-    global.mainWindow.loadURL('http://localhost:3000/');
-  else {
-    global.mainWindow.loadURL(url.format({
-      pathname: path.join(__dirname, 'build', 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    }));
+    if (process.env.NODE_ENV === 'development')
+      global.mainWindow.loadURL('http://localhost:3000/');
+    else {
+      global.mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'build', 'index.html'),
+        protocol: 'file:',
+        slashes: true
+      }));
+    }
+
+    // global.mainWindow.webContents.openDevTools();
+
+    // listen "proc-with-tracking-number" event
+    ipcMain.on('proc-with-tracking-number', async function(event, trackingNum, email) {
+      try {
+        // get receipt information by tracking number
+        var receiptInfo = await receiptRetriever.byTrackingNum(trackingNum, email);
+        await checkReceipt(receiptInfo);
+
+        setTimeout(function() {
+          // send "proc-finish" event
+          event.sender.send('proc-finish', null);
+          global.mainWindow.close();
+        }, 1000);
+      } catch (err) {
+        if (err.code === pfValues.errors.readerNotFound.code) {
+          dialog.showMessageBox(global.mainWindow, {
+            type: 'none',
+            message: 'This program requires you install Acrobat Reader first.',
+            buttons: ['OK']
+          });
+          event.sender.send('proc-finish', null);
+        } else if (err.code === pfValues.errors.readerIsOpening.code) {
+          dialog.showMessageBox(global.mainWindow, {
+            type: 'none',
+            message: 'This program requires you close Acrobat Reader first.',
+            buttons: ['OK']
+          });
+          event.sender.send('proc-finish', null);
+        } else {
+          console.log(err);
+          event.sender.send('proc-finish', err);
+        }
+      }
+    });
+
+    // listen "proc-with-local-file" event
+    ipcMain.on('proc-with-local-file', async function(event, filePath) {
+      try {
+        // get receipt information by file path
+        var receiptInfo = await receiptRetriever.byLocalFile(filePath);
+        await checkReceipt(receiptInfo);
+
+        setTimeout(function() {
+          // send "proc-finish" event
+          event.sender.send('proc-finish', null);
+          global.mainWindow.close();
+        }, 1000);
+      } catch (err) {
+        if (err.code === pfValues.errors.readerNotFound.code) {
+          dialog.showMessageBox(global.mainWindow, {
+            type: 'none',
+            message: 'This program requires you install Acrobat Reader first.',
+            buttons: ['OK']
+          });
+          event.sender.send('proc-finish', null);
+        } else if (err.code === pfValues.errors.readerIsOpening.code) {
+          dialog.showMessageBox(global.mainWindow, {
+            type: 'none',
+            message: 'This program requires you close Acrobat Reader first.',
+            buttons: ['OK']
+          });
+          event.sender.send('proc-finish', null);
+        } else {
+          event.sender.send('proc-finish', err);
+        }
+      }
+    });
+
+    global.mainWindow.on('closed', function() {
+      global.mainWindow = null;
+    });
   }
-
-  // global.mainWindow.webContents.openDevTools();
-
-  // listen "proc-with-tracking-number" event
-  ipcMain.on('proc-with-tracking-number', async function(event, trackingNum, email) {
-    try {
-      // get receipt information by tracking number
-      var receiptInfo = await receiptRetriever.byTrackingNum(trackingNum, email);
-      await checkReceipt(receiptInfo);
-
-      setTimeout(function() {
-        // send "proc-finish" event
-        event.sender.send('proc-finish', null);
-        global.mainWindow.close();
-      }, 1000);
-    } catch (err) {
-      if (err.code === pfValues.errors.readerNotFound.code) {
-        dialog.showMessageBox(global.mainWindow, {
-          type: 'none',
-          message: 'This program requires you install Acrobat Reader first.',
-          buttons: ['OK']
-        });
-        event.sender.send('proc-finish', null);
-      } else if (err.code === pfValues.errors.readerIsOpening.code) {
-        dialog.showMessageBox(global.mainWindow, {
-          type: 'none',
-          message: 'This program requires you close Acrobat Reader first.',
-          buttons: ['OK']
-        });
-        event.sender.send('proc-finish', null);
-      } else {
-        console.log(err);
-        event.sender.send('proc-finish', err);
-      }
-    }
-  });
-
-  // listen "proc-with-local-file" event
-  ipcMain.on('proc-with-local-file', async function(event, filePath) {
-    try {
-      // get receipt information by file path
-      var receiptInfo = await receiptRetriever.byLocalFile(filePath);
-      await checkReceipt(receiptInfo);
-
-      setTimeout(function() {
-        // send "proc-finish" event
-        event.sender.send('proc-finish', null);
-        global.mainWindow.close();
-      }, 1000);
-    } catch (err) {
-      if (err.code === pfValues.errors.readerNotFound.code) {
-        dialog.showMessageBox(global.mainWindow, {
-          type: 'none',
-          message: 'This program requires you install Acrobat Reader first.',
-          buttons: ['OK']
-        });
-        event.sender.send('proc-finish', null);
-      } else if (err.code === pfValues.errors.readerIsOpening.code) {
-        dialog.showMessageBox(global.mainWindow, {
-          type: 'none',
-          message: 'This program requires you close Acrobat Reader first.',
-          buttons: ['OK']
-        });
-        event.sender.send('proc-finish', null);
-      } else {
-        event.sender.send('proc-finish', err);
-      }
-    }
-  });
-
-  global.mainWindow.on('closed', function() {
-    global.mainWindow = null;
-  });
 }
 
 // make application as single instance
